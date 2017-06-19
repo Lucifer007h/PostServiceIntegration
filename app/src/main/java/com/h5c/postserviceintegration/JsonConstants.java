c7acc14d0b402d19b6b62a75dd995b2f54312182
Harsh B. Gupta <harsh.gupta1602@gmail.com>
2017-06-19 18:11:52 +0530

    Update JsonConstants.java

--- app/src/main/java/com/h5c/postserviceintegration/JsonConstants.java
+++ app/src/main/java/com/h5c/postserviceintegration/JsonConstants.java
@@ -6,12 +6,6 @@ package com.h5c.postserviceintegration;
 public class JsonConstants {
 
     public static final String BASE_URL = "http://serverip:PortNO"; 
-
-// userName == devdoctor , password == devdoctor
-
-    //public static final String BASE_URL ="http://103.19.89.152:8080";
-    //public static final String BASE_URL = "http://192.168.1.141:8080";
-    //public static final String BASE_URL = "http://192.168.1.138:8080";
     public static final String LOGIN_URL = "RemainingPartOfServiceOfURLAfterBaseURL";
 }
 
